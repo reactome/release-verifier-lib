7178fa5d4641bb9b76f5da27d0bebdb44354f1a0
Joel Weiser <jweiser13@protonmail.com>
2024-12-12 11:59:44 -0500

    overloads count functions to use long type params

--- src/main/java/org/reactome/release/verifier/CountUtils.java
+++ src/main/java/org/reactome/release/verifier/CountUtils.java
@@ -15,6 +15,18 @@ public class CountUtils {
      * @return <code>true</code> if five percent or more drop;<code>false</code> otherwise
      */
     public static boolean greaterThanOrEqualTo5PercentDrop(int newCount, int oldCount) {
+        return greaterThanOrEqualTo5PercentDrop((long) newCount, (long) oldCount);
+    }
+
+    /**
+     * Checks if the difference between two counts is a reduction of five percent or more.  If the drop is less (or
+     * the counts are equal or there is an increase), the method returns false.
+     *
+     * @param newCount More recent count
+     * @param oldCount Less recent count
+     * @return <code>true</code> if five percent or more drop;<code>false</code> otherwise
+     */
+    public static boolean greaterThanOrEqualTo5PercentDrop(long newCount, long oldCount) {
         final int percentage = 5;
         return greaterThanOrEqualToXPercentDrop(newCount, oldCount, percentage);
     }
@@ -29,6 +41,19 @@ public class CountUtils {
      * @return <code>true</code> if "percentage" or more drop;<code>false</code> otherwise
      */
     public static boolean greaterThanOrEqualToXPercentDrop(int newCount, int oldCount, int percentage) {
+        return greaterThanOrEqualToXPercentDrop((long) newCount, (long) oldCount, percentage);
+    }
+
+    /**
+     * Checks if the difference between two counts is a reduction of "percentage" percent or more.
+     * If the drop is less (or the counts are equal or there is an increase), the method returns false.
+     *
+     * @param newCount More recent count
+     * @param oldCount Less recent count
+     * @param percentage Percentage drop
+     * @return <code>true</code> if "percentage" or more drop;<code>false</code> otherwise
+     */
+    public static boolean greaterThanOrEqualToXPercentDrop(long newCount, long oldCount, int percentage) {
         double percentChange = (newCount - oldCount) * 100.0d / oldCount;
 
         return (percentChange < 0 && Math.abs(percentChange) >= percentage);
